adb562d0fc31a663bc5dd39b6e7806d13ff5b9f9
kevin-lth <59739294+kevin-lth@users.noreply.github.com>
2020-03-02 22:37:23 +0000

    Fixed grammar

--- music/common/models.mjs
+++ music/common/models.mjs
@@ -4,7 +4,7 @@ export function newAccount(name, storedPassword) {
 
 export function newCategory(reference, name, shortName, childrenCategories, coverReference) {
     // childrenCategories is supposed to be a list of categories.
-    // It MAY be undefined if this list is unimportant (e.g if we add a new category to the database) , but NEVER null
+    // It MAY be undefined if this list is unimportant (e.g. if we add a new category to the database), but NEVER null
     return { reference, name, shortName, childrenCategories, coverReference };
 }
 
