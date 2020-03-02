f53edeea7bfdfd0f653fa49cfd8db21e63014cfe
kevin-lth <59739294+kevin-lth@users.noreply.github.com>
2020-03-02 22:36:49 +0000

    Updated category model

--- music/common/models.mjs
+++ music/common/models.mjs
@@ -3,6 +3,8 @@ export function newAccount(name, storedPassword) {
 }
 
 export function newCategory(reference, name, shortName, childrenCategories, coverReference) {
+    // childrenCategories is supposed to be a list of categories.
+    // It MAY be undefined if this list is unimportant (e.g if we add a new category to the database) , but NEVER null
     return { reference, name, shortName, childrenCategories, coverReference };
 }
 
