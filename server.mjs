6178f4500c34cd4ae92aa66ffd2d07a65c056664
kevin-lth <59739294+kevin-lth@users.noreply.github.com>
2020-03-04 20:49:58 +0000

    Added support for HTTP2 and TLS Encryption

--- server.mjs
+++ server.mjs
@@ -1,17 +1,27 @@
+import * as HTTP2 from 'http2';
+import * as fs from 'fs';
+
 import * as music from './music/main.mjs';
 
+// Please replace both the key and the certificate with ones provided by a Certificate Authority if used on a production server
+// Self-signed certificate are acceptable for development purposes only
+let options = {
+    // Synchronous file read is intentional here, as we cannot start the server without having both the key and the certificate
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+
 // Start the server on this port
-import * as HTTP from 'http';
-start(8180);
+start(8180, options);
 
 // Provide a service to localhost only.
-function start(port) {
-  let service = HTTP.createServer(handle);
-  service.listen(port, 'localhost');
-  console.log("Visit localhost:" + port);
+function start(port, options) {
+    let service = HTTP2.createSecureServer(options, handle);
+    service.listen(port, 'localhost');
+    console.log("Server started at localhost:" + port);
 }
 
 // Deal with a request by redirecting everything to the music module. This may be modified to only redirect to this module on a specific URL for instance
 async function handle(request, response) {
-  await music.handle(request, response);
+    await music.handle(request, response);
 }
