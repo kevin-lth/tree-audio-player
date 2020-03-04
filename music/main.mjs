01137ab27d36ccf3fe088e8bb2d2af444f2dea59
kevin-lth <59739294+kevin-lth@users.noreply.github.com>
2020-03-04 20:50:29 +0000

    Fixed URL Handling, added error when URL is wrong

--- music/main.mjs
+++ music/main.mjs
@@ -4,7 +4,7 @@ import { newURL } from './utils.mjs';
 import { testAll } from './tests.mjs';
 import * as api from './api/main.mjs'
 
-let OK = 200, badRequest = 400;
+let OK = 200, badRequest = 400, forbidden = 403, notFound = 404;
 
 // Deal with a request.
 export async function handle(request, response) {
@@ -14,9 +14,22 @@ export async function handle(request, response) {
         error(badRequest, response);
         return;
     }
-    if (url[0] === 'api') {
-        // We redirect the request to the API
-        // TODO
+    // Node already lower cases the header, as well as remove duplicate. Nothing to do in that regard
+    
+    switch (url.paths[0]) {
+        case '':
+            // We know from URL validation that this HAS to be the last fragment of the URL if it is empty
+            // In other words, the URL has to be '/'
+            console.log('main');
+            break;
+        case 'api':
+            // We redirect the request to the API
+            // TODO
+            console.log('api');
+            break;
+        default:
+            error(notFound, response);
+            return; // No break, as it is unaccessible anyway
     }
     console.log("[Request (Music)] Method:", request.method);
     console.log("[Request (Music)] URL:", request.url);
