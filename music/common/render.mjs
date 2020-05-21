e1bbaa0da5aebac462d04e7bc96783b136b8f84d
kevin-lth <kevin.lethuillier@gmail.com>
2020-05-21 14:24:02 +0200

    Readded indentation as it doesn't impact XHTML validation

--- music/common/render.mjs
+++ music/common/render.mjs
@@ -54,40 +54,45 @@ export function newRender(bindings) {
         const result = await Promise.all([renderHeader(token), renderNavs(token, selected_page), renderFooter()]); // We use Promise.all to allow both promises to happen simultaneously, which wouldn't be possible with merely await
         const header = result[0], navs = result[1], footer = result[2];
         return `<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB" xml:lang="en-GB">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>${title_prefix} - ${title_suffix}</title>
-        <link rel="preload" href="/assets/main.css" as="style" />
-        <link rel="preload" href="/assets/main.js" as="script" />
-        <link rel="preload" href="/assets/logo.svg" as="image" />
-        
-        <link rel="icon" href="/assets/logo.svg" type="image/svg+xml" />
-        
-        <link rel="stylesheet" href="/assets/main.css" />
-    </head>
-    <body>
-        <header class="header">${header}</header>
-        <nav>
-            <div class="desktop-nav">${navs.desktop}</div>
-            <div class="mobile-nav">${navs.mobile}</div>
-        </nav>
-        <main class="main">${main}</main>
-        <footer class="footer">${footer}</footer>
-        <script src="/assets/main.js"></script> <!-- Executing the script before would slow the first paint of the page -->
-    </body>
-</html>`;
+                <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB" xml:lang="en-GB">
+                    <head>
+                        <meta charset="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>${title_prefix} - ${title_suffix}</title>
+                        <link rel="preload" href="/assets/main.css" as="style" />
+                        <link rel="preload" href="/assets/main.js" as="script" />
+                        <link rel="preload" href="/assets/logo.svg" as="image" />
+                        
+                        <link rel="icon" href="/assets/logo.svg" type="image/svg+xml" />
+                        
+                        <link rel="stylesheet" href="/assets/main.css" />
+                    </head>
+                    <body>
+                        <header class="header">${header}</header>
+                        <nav>
+                            <div class="desktop-nav">${navs.desktop}</div>
+                            <div class="mobile-nav">${navs.mobile}</div>
+                        </nav>
+                        <main class="main">${main}</main>
+                        <footer class="footer">${footer}</footer>
+                        <script src="/assets/main.js"></script> <!-- Executing the script before would slow the first paint of the page -->
+                    </body>
+                </html>`;
     }
     
     // TODO ; add login/logout button
     async function renderHeader(token) {
         const session_status = await bindings.getSessionStatus(token);
         let username = 'visitor';
-        if (session_status.http_code === HTTP_OK && session_status.response.username !== null) { username = `<span class="header-username">${session_status.response.username}</span>`; }
+        let login_or_logout = '<a href="/html/login/" class="header-login">Login</a>';
+        if (session_status.http_code === HTTP_OK && session_status.response.username !== null) { 
+            username = `<span class="header-username">${session_status.response.username}</span>`;
+            login_or_logout = '<span class="header-logout">Logout</span>'; // We don't use a form with POST because that would redirect to the API, which we don't want. We will handle logout with javascript.
+        }
         return `<a href="/html" class="header-logo"><img src="/assets/logo.svg" alt="Tree with a music note" /></a>
-<span class="header-title">Tree Audio Player</span>
-<span class="header-hello">Hello, <span class="header-username">${username}</span> !</span>`;
+                <span class="header-title">Tree Audio Player</span>
+                <span class="header-hello">Hello, ${username} !</span>
+                ${login_or_logout}`;
     }
     
     function renderNavElement(page, selected_page, href, svg_link, text, show_text) {
@@ -98,24 +103,21 @@ export function newRender(bindings) {
         return `<li class="nav-element ${nav_active}"><a href="${href}"><img src="${svg_link}" alt="${text}" />${visible_text}</a></li>`;
     }
     
+    // TODO: SVG Icons for nav elements
     async function renderNavElements(token, selected_page, show_text) {
         const session_status = await bindings.getSessionStatus(token);
+        let user_only_elements = '';
         if (session_status.http_code === HTTP_OK && session_status.response.username !== null) { 
-            return `<ul>
- ${renderNavElement('home', selected_page, '/html/', '', 'Home', show_text)}
- ${renderNavElement('category_public', selected_page, '/html/category/public/', '', 'Public Categories', show_text)}
- ${renderNavElement('category_personal', selected_page, '/html/category/personal/', '', 'Personal Categories', show_text)}
- ${renderNavElement('playlist', selected_page, '/html/playlist/', '', 'Playlist', show_text)}
- ${renderNavElement('settings', selected_page, '/html/settings/', '', 'Settings', show_text)}
- ${renderNavElement('about', selected_page, '/html/about/', '', 'About', show_text)}
-</ul>`;
-        } else {
-            return `<ul>
- ${renderNavElement('home', selected_page, '/html/', '', 'Home', show_text)}
- ${renderNavElement('settings', selected_page, '/html/settings/', '', 'Settings', show_text)}
- ${renderNavElement('about', selected_page, '/html/about/', '', 'About', show_text)}
-</ul>`;
+            user_only_elements = `${renderNavElement('category_public', selected_page, '/html/category/public/', '', 'Public Categories', show_text)}
+                                  ${renderNavElement('category_personal', selected_page, '/html/category/personal/', '', 'Personal Categories', show_text)}
+                                  ${renderNavElement('playlist', selected_page, '/html/playlist/', '', 'Playlist', show_text)}`;
         }
+        return `<ul>
+                    ${renderNavElement('home', selected_page, '/html/', '', 'Home', show_text)}
+                    ${user_only_elements}
+                    ${renderNavElement('settings', selected_page, '/html/settings/', '', 'Settings', show_text)}
+                    ${renderNavElement('about', selected_page, '/html/about/', '', 'About', show_text)}
+               </ul>`;
     }
     
     async function renderDesktopNav(token, selected_page) {
